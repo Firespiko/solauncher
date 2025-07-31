65f364df2a0a57d56300186e98c096ffca536517
sreevarshan-xenoz <sreevarshan1511@gmail.com>
2025-07-31 21:33:28 +0530

    Set up project structure and core interfaces

---
+++ src/modules/index.ts
@@ -0,0 +1,7 @@
+// Main modules export
+// Provides access to all platform modules
+
+export * from './ico';
+export * from './airdrop';
+export * from './liquidity-pools';
+export * from './portfolio';
